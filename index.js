0de8ff34f989db8b270ec5fabfa0356af4acb84f
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-08-17 11:40:19 +0200

    modify pagination

--- index.js
+++ index.js
@@ -11,27 +11,49 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = `${info.pages}`;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 async function fetchCharacters() {
   const response = await fetch(
-    `"https://rickandmortyapi.com/api/character?page=${info.pages}"`
+    `https://rickandmortyapi.com/api/character?page=<page${page}>`
   );
   const json = await response.json();
-  console.log(json.results);
+  maxPage = json.info.pages;
   return json.results;
 }
-
-fetchCharacters();
+function paginationDisplay() {
+  pagination.textContent = `${page}/${maxPage}`;
+}
+// fetchCharacters();
 
 async function moreCards() {
-  const results = await fetchCharacters();
+  const results = await fetchCharacters(page);
+  cardContainer.innerHTML = "";
   results.forEach((result) => {
     const newCard = createCharacterCard(result);
     cardContainer.innerHTML += newCard;
   });
+  paginationDisplay();
 }
 
 moreCards();
+
+nextButton.addEventListner("click", () => {
+  if (page <= maxPage) {
+    page++;
+    moreCards();
+    fetchCharacters();
+  }
+});
+
+prevButton.addEventListner("click", () => {
+  if (page > 1) {
+    page--;
+    moreCards();
+    fetchCharacters();
+  }
+});
+
+paginationDisplay();
